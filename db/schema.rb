aa64b6f1f6e822892e9b30ea4f620c01d5248eef
Reid Stivers <reidstivers@gmail.com>
2024-08-01 20:24:17 +0900

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_01_103722) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_01_111603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "pantry_id", null: false
+    t.float "amount"
+    t.string "unit"
+    t.date "expiration_date"
+    t.string "category"
+    t.boolean "in_pantry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pantry_id"], name: "index_ingredients_on_pantry_id"
+  end
+
+  create_table "pantries", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +44,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_01_103722) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "ingredients", "pantries"
 end
