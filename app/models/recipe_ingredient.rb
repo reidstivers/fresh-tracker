4bb7366b26c7386649e01e7457b23ffcaccbcb0c
Kurtz <jonathankrtz@gmail.com>
2024-08-03 17:13:18 +0900

    Implemented category models, added id_migration to ingredients/recipe_ingredients, built iterations to render appropriate buttons & divs

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,6 +2,7 @@ class RecipeIngredient < ApplicationRecord
   belongs_to :ingredient
   belongs_to :recipe
   belongs_to :shopping_list
+  belongs_to :category
   validates :amount, presence: true
   validates :unit, presence: true
 end
