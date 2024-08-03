1f8cf5fba682276087b5c7a29ca411e4afd30f92
Reid Stivers <reidstivers@gmail.com>
2024-08-03 14:49:46 +0900

    still working on getting the ocr working but I have the beginning of a framework

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+# Our gems
+gem 'rtesseract'
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,46 @@
 class IngredientsController < ApplicationController
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @pantry = current_user.pantry
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.pantry = @pantry
+    @ingredient.in_pantry = true
+
+    if @ingredient.save
+      redirect_to pantry_path(@pantry), notice: "Ingredient saved"
+    else
+      render "pantries/show", notice: "Ingredient not saved"
+    end
+  end
+
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def edit
+    @ingredient = Ingredient.find(params[:id])
+  end
+
+  def update
+    @ingredient = Ingredient.find(params[:id])
+
+    if @ingredient.update(ingredient_params)
+      redirect_to @ingredient
+    else
+      render "pantry/show", notice: "Ingredient not updated"
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :amount, :unit, :expiration_date, :category, :in_pantry, :pantry_id)
+  end
 end
