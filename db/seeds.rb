6c365eaa227529e70a90811521656100daba44fe
Chiaki Hayashi <chiaki0607@gmail.com>
2024-08-03 14:32:26 +0900

    some seeds for ingredients.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# puts "Cleaning database..."
+# Restaurant.destroy_all
+require "date"
+today = Date.today
+futuredate = Date.new(2024, 8, 14)
+
+puts "Checking if Pantry exists ..."
+Pantry.create! if Pantry.all.count.zero?
+
+pantry = Pantry.last
+pantry.id += 1 if pantry.id != 1
+puts "Pantry id is #{pantry.id}"
+
+puts "Creating ingredients..."
+wholemilk = { name: "Whole Milk", pantry_id: pantry.id, amount: 1, unit: "carton", expiration_date: today, category: "dairy", in_pantry: true }
+broccori = { name: "Broccoli", pantry_id: pantry.id, amount: 2, unit: "", expiration_date: today, category: "vegitables", in_pantry: true }
+carrot = { name: "Carrot", pantry_id: pantry.id, amount: 3, unit: "", expiration_date: futuredate, category: "vegitables", in_pantry: true }
+kobebeef = { name: "Kobe beef steak", pantry_id: pantry.id, amount: 2, unit: "slice", expiration_date: futuredate, category: "meat", in_pantry: true }
+orangejuice = { name: "Orange juice", pantry_id: pantry.id, amount: 500, unit: "ml", expiration_date: futuredate, category: "beverage", in_pantry: true }
+
+[wholemilk, broccori, carrot, kobebeef, orangejuice].each do |attributes|
+  ingredient = Ingredient.create!(attributes)
+  puts "Created #{ingredient.name}"
+end
+puts "Finished!"
