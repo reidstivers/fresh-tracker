556a16ac23134834a4143637777974d317148d89
Kurtz <jonathankrtz@gmail.com>
2024-08-06 11:19:08 +0900

    Fixed issue with whole page turning red. Aligned an edit icon on the ingredient card.

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -34,7 +34,7 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.find(params[:id])
 
     if @ingredient.update(ingredient_params)
-      redirect_to @ingredient
+      redirect_to @ingredients, notice: "Ingredient updated"
     else
       render "pantry/show", notice: "Ingredient not updated"
     end
