fdacea84ded02df7ab8c13a5a7a2cdcad6a178c0
Chiaki Hayashi <chiaki0607@gmail.com>
2024-08-06 21:37:30 +0900

    shopping list route added

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   # root "posts#index"
   # Route for pantries. user only needs to see their own pantry. Ingredients managed through that pantry.
   resources :ingredients
+  resources :shopping_lists, path: 'shoppinglist'
 end
