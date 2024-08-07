019310d2a9bf2d76809f35828867988378909cf8
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-08-08 01:32:12 +0900

    Shopping list now linked to pantry not user and added to navbar

--- app/models/pantry.rb
+++ app/models/pantry.rb
@@ -1,4 +1,5 @@
 class Pantry < ApplicationRecord
   has_many :users
   has_many :ingredients
+  has_one :shopping_list
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_120501) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_07_155243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,10 +66,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_120501) do
   end
 
   create_table "shopping_lists", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_shopping_lists_on_user_id"
+    t.bigint "pantry_id"
+    t.index ["pantry_id"], name: "index_shopping_lists_on_pantry_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,6 +94,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_120501) do
   add_foreign_key "recipe_ingredients", "recipes"
   add_foreign_key "recipe_ingredients", "shopping_lists"
   add_foreign_key "recipes", "users"
-  add_foreign_key "shopping_lists", "users"
+  add_foreign_key "shopping_lists", "pantries"
   add_foreign_key "users", "pantries"
 end
