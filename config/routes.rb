56d1c266eae3cbc056391f3618253e3aa081be58
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-08-13 21:14:20 +0900

    Image API

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Route for pantries. user only needs to see their own pantry. Ingredients managed through that pantry.
   resources :ingredients
   resources :shopping_lists, path: 'shoppinglist'
+  post 'images/recognize', to: 'images#recognize'
 end
