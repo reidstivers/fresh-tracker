c1075d571b6ca30340c30a1fa22937423ba6b46e
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-08-13 17:54:43 +0900

    Javascript should be fixed

--- app/assets/javascripts/swipe.js
+++ app/assets/javascripts/swipe.js
@@ -1,36 +1,37 @@
 function setupSwipe() {
-    const cards = document.querySelectorAll('.shopping-list-card');
+  let touchstartX = 0;
+  let touchendX = 0;
 
-    cards.forEach(card => {
-        let touchstartX = 0;
-        let touchendX = 0;
+  document.addEventListener('touchstart', e => {
+      if (e.target.closest('.shopping-list-card')) {
+          touchstartX = e.changedTouches[0].screenX;
+      }
+  }, false);
 
-        card.addEventListener('touchstart', e => {
-            touchstartX = e.changedTouches[0].screenX;
-        }, false);
+  document.addEventListener('touchend', e => {
+      const card = e.target.closest('.shopping-list-card');
+      if (card) {
+          touchendX = e.changedTouches[0].screenX;
+          if (touchstartX > touchendX + 50) {
+              handleClickEvent(card);
+          }
+      }
+  }, false);
 
-        card.addEventListener('touchend', e => {
-            touchendX = e.changedTouches[0].screenX;
-            if (touchstartX > touchendX + 50) { // Swipe left
-                handleClickEvent(card);
-            }
-        }, false);
-    });
+  function handleClickEvent(card) {
+      const checkButton = card.querySelector('.fa-check-to-slot');
+      handleSwipeLeft(card);
+      if (checkButton) {
+          checkButton.click();
+      }
+  }
 
-    function handleClickEvent(card) {
-        const checkButton = card.querySelector('.fa-check-to-slot');
-        handleSwipeLeft(card);
-        if (checkButton) {
-            checkButton.click();
-        }
-    }
-
-    function handleSwipeLeft(card) {
-        card.classList.add('swipe-left');
-        setTimeout(() => {
-            card.style.display = 'none';
-        }, 1000);
-    }
+  function handleSwipeLeft(card) {
+      card.classList.add('swipe-left');
+      setTimeout(() => {
+          card.style.display = 'none';
+      }, 1000);
+  }
 }
 
-document.addEventListener('DOMContentLoaded', setupSwipe);
+setupSwipe();
