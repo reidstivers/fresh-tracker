02e3dcfa782556273392ab77b9cef813beefdd4b
Kurtz <jonathankrtz@gmail.com>
2024-08-14 10:46:42 +0900

    Added validation to prevent ingredient name from getting too long

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,7 @@ class Ingredient < ApplicationRecord
   belongs_to :pantry
   belongs_to :category
   has_many :recipe_ingredients
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
   validates :category_id, presence: true
   validates :amount, presence: true
   validates :in_pantry, presence: true
